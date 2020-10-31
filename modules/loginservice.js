f9a6a39879d2ff34158bab4ac08195509e798833
PasamPavithra <pasampavithra27@gmail.com>
2020-10-31 22:40:27 +0530

    update

--- modules/loginservice.js
+++ modules/loginservice.js
@@ -1,14 +1,18 @@
 class LoginService {
-    login = function(email, password, user){
+    login = function(email, password, role){
         var usersTemp = JSON.parse(localStorage.getItem("USERS"));
         var users= usersTemp ? usersTemp: [];     
         let exists = false;   
         for(let obj of users)
         {
-            if( obj.email == email  && obj.password == password && obj.role == user)
+            if( obj.email == email  && obj.password == password && obj.user == role)
             {
                 exists=true;
-                localStorage.setItem("LOGGED_IN_USER",JSON.stringify(obj));
+                var users=[];
+                var usersTemp = JSON.parse(localStorage.getItem("LOGGED_IN_USER"));
+                var users= usersTemp ? usersTemp:[];
+                localStorage.setItem("LOGGED_IN_USER", JSON.stringify(obj ) );
+                localStorage.getItem("LOGGED_IN_USER");
                 break;
             }
         }
