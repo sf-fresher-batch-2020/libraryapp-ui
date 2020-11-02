bc5ab4b23dfa9d77a79c4d50178f59887fa241b7
PasamPavithra <pasampavithra27@gmail.com>
2020-11-02 23:07:51 +0530

    updated

---
+++ modules/borrowBookService.js
@@ -0,0 +1,10 @@
+class borrowBookService{
+    borrowbook = function(borrowlist){
+       var usersTemp = JSON.parse(localStorage.getItem("BORROWBOOK"));
+       var users= usersTemp ? usersTemp:[];
+       users.push(borrowlist);
+       localStorage.setItem("BORROWBOOK",JSON.stringify(users));
+       let result="success";
+       return result;
+        }
+}
