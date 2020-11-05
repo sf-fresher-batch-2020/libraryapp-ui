7948474e31a9e5da3e0f3d5aa598a45aa4c95a38
PasamPavithra <pasampavithra27@gmail.com>
2020-11-05 22:10:06 +0530

    updated

---
+++ modules/returnbooklist.js
@@ -0,0 +1,19 @@
+class ReturnBookListService 
+{
+    
+    save = function(obj){
+        
+        var temp = JSON.parse(localStorage.getItem("BORROWBOOK"));
+        var list = temp ? temp : [];
+        list.push(obj);       
+        localStorage.setItem("BORROWBOOK", JSON.stringify(list ) );
+    }
+    list = function(){
+        var temp = JSON.parse(localStorage.getItem("BORROWBOOK"));
+        var list = temp ? temp : [];
+        return list;
+    }
+
+    
+
+}
