a85f70a98df7f3611d01b1e1925a52eae8834b54
PasamPavithra <pasampavithra27@gmail.com>
2020-11-24 18:58:43 +0530

    updated register validation

--- modules/regsiterservice.js
+++ modules/regsiterservice.js
@@ -1,10 +1,16 @@
 class RegisterService {
     register = function(userObj) {
-        var usersTemp = JSON.parse(localStorage.getItem("USERS"));
-        var users = usersTemp ? usersTemp : [];
-        users.push(userObj);
-        localStorage.setItem("USERS", JSON.stringify(users));
-        let result = "success";
-        return result;
+        var usersr = JSON.parse(localStorage.getItem("USERS")) || [];
+        if (!usersr.some(user => user.email === userObj.email)) {
+            usersr.push(userObj);
+            localStorage.setItem("USERS", JSON.stringify(usersr));
+            location.reload();
+            console.log(userObj);
+            alert("continue login");
+            let result = "success";
+            return result;
+        } else {
+            alert("user already exists try registering with different credentials");
+        }
     }
 }
